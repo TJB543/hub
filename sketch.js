7801ccac1ec2fd690ba6681571c097f9f9b96b7f
TJB543 <58111256+TJB543@users.noreply.github.com>
2019-12-21 15:08:04 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,31 @@
 function setup() {
     createCanvas(windowWidth,windowHeight);
-    Bring_Your_Brolly = createButton("Bring Your Brolly")
-    Bring_Your_Brolly.position(width/4,height/4)
+    Bring_Your_Brolly = createButton("Bring Your Brolly");
+    Bring_Your_Brolly.position(width/4-Bring_Your_Brolly.width/2,height/4);
+    Flavoball = createButton("Flavoball");
+    Flavoball.position(width/4*3-Flavoball.width/2,height/4);
+    Stick_war = createButton("Pre-release: Stick war")
+    Stick_war.position(width/2-Stick_war.width/2,height/2)
 }
 
 function BYB_URL() {
     window.open("https://tjb543.github.io/Bring-Your-Brolly/")
 }
 
+function FLAVO_URL() {
+    window.open("https://lsiret.github.io/Flavoball/");
+}
+
+function SW_URL(){
+    window.open("https://tjb543.github.io/stick-war/")
+}
+
 function draw() {
-    background(0);
+    background(255);
+    textAlign(CENTER);
+    text("Welcome to Tobey & Louis gaming hub!",width/2,height/8)
+    text("Please choose a game from below!",width/2,height/8+20)
     Bring_Your_Brolly.mousePressed(BYB_URL);
+    Flavoball.mousePressed(FLAVO_URL);
+    Stick_war.mousePressed(SW_URL)
 }
